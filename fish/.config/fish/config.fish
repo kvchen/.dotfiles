915b595f0b90bb23e23578b2991931022e0dbf90
Kevin Chen <keffcat@gmail.com>
2016-06-08 07:38:31 -0700

    Update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,11 +4,11 @@ set fish_greeting ""
 
 
 # Set environment variables
+
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
 
 set -x GAE_SDK /usr/local/Cellar/app-engine-python/1.9.27/share/google-app-engine
-set -gx PATH /usr/local/bin /usr/texbin $PATH
-set -gx PYTHONPATH /usr/local/Cellar/opencv3/3.1.0_3/lib/python3.5/site-packages $PYTHONPATH
+# set -gx PATH /usr/local/bin /usr/texbin $PATH
 
 
 # Aliases
@@ -17,10 +17,12 @@ alias vim="nvim"
 alias py="ptpython"
 
 
-# For faster shitposting
+# Commands on startup
 
-alias goodshit="echo '👌👀👌👀👌👀👌👀👌👀 good shit go౦ԁ sHit👌 thats ✔ some good👌👌shit right👌👌there👌👌👌 right✔there ✔✔if i do ƽaү so my self 💯 i say so 💯 thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯 👌 👀 👀 👀 👌👌Good shit'"
-alias lennyface="echo '( ͡° ͜ʖ ͡°)'"
-alias cracksheen="echo 'https://i.imgur.com/4ncvZH7.gif'"
-alias lowqualitybait="echo 'https://i.imgur.com/EjJopdb.jpg'"
+function cl_vi_key_bindings
+    fish_vi_key_bindings
+    bind -M insert \cl 'clear; commandline -f repaint'
+end
+
+set -g fish_key_bindings cl_vi_key_bindings
 
